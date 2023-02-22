918b1b53ae2cbc747c9aa8c56b90d2defa326931
Rudolfs Pumpurs <rudolfs.pumpurs@gmail.com>
2023-02-22 17:10:34 +0200

    Add Bear and Steady Gene speed improvements

--- bear-and-steady-gene/code.php
+++ bear-and-steady-gene/code.php
@@ -21,6 +21,40 @@ function steadyGene($gene) {
         $countsOriginal[$gene[$i]]++;
     }
 
+    $deviationPos = 0;
+    $deviationNeg = 0;
+    $deviationA = $steadyCount - $countsOriginal['A'];
+    if ($deviationA > 0) {
+        $deviationPos += $deviationA;
+    } else {
+        $deviationNeg += $deviationA;
+    }
+    $deviationC = $steadyCount - $countsOriginal['C'];
+    if ($deviationC > 0) {
+        $deviationPos += $deviationC;
+    } else {
+        $deviationNeg += $deviationC;
+    }
+    $deviationG = $steadyCount - $countsOriginal['G'];
+    if ($deviationG > 0) {
+        $deviationPos += $deviationG;
+    } else {
+        $deviationNeg += $deviationG;
+    }
+    $deviationT = $steadyCount - $countsOriginal['T'];
+    if ($deviationT > 0) {
+        $deviationPos += $deviationT;
+    } else {
+        $deviationNeg += $deviationT;
+    }
+//    echo 'deviationA ' . $deviationA . PHP_EOL;
+//    echo 'deviationC ' . $deviationC . PHP_EOL;
+//    echo 'deviationG ' . $deviationG . PHP_EOL;
+//    echo 'deviationT ' . $deviationT . PHP_EOL;
+//    echo 'deviationPos ' . $deviationPos . PHP_EOL;
+//    echo 'deviationNeg ' . $deviationNeg . PHP_EOL;
+//    die();
+
     if ($countsOriginal['A'] === $steadyCount
         && $countsOriginal['C'] === $steadyCount
         && $countsOriginal['G'] === $steadyCount
@@ -31,7 +65,7 @@ function steadyGene($gene) {
 
     $min = $n;
     for ($i = 0; $i < $n; $i++) {
-        //echo $i . PHP_EOL;
+        echo $i . ' ' . $min . PHP_EOL;
         $countsChanged = [
             'A' => 0,
             'C' => 0,
@@ -39,8 +73,14 @@ function steadyGene($gene) {
             'T' => 0,
         ];
         for ($j = $i; $j < $n; $j++) {
-            $countsChanged[$gene[$j]]++;
             $substringLength = $j - $i + 1;
+            if ($substringLength >= $min) {
+                break;
+            }
+            $countsChanged[$gene[$j]]++;
+            if ($substringLength < $deviationPos) {
+                continue;
+            }
 
             $missingACount = $steadyCount - ($countsOriginal['A'] - $countsChanged['A']);
             if ($missingACount < 0) {
